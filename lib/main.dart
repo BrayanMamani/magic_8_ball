cb844d96aa8e10ab78461579140e7117c4d0396c
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-06-11 23:22:21 -0400

    Add class

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,24 @@ class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+        ),
+        body: Ball(),
+      ),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
