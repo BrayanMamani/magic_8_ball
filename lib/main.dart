98f96ae1b59b40d3424f792a5f25a33446e64ff0
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-06-11 23:18:46 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BallPage());
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
